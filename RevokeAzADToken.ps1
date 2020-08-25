e168473d55f2a9e3128c6588d250fdac54e9e7b6
entish-tyler <58454853+entish-tyler@users.noreply.github.com>
2020-08-24 21:11:14 -0600

    Update RevokeAzADToken.ps1

--- RevokeAzADToken.ps1
+++ RevokeAzADToken.ps1
@@ -2,19 +2,23 @@
 
 param(
     [Parameter(Mandatory=$True)]
-    [Alias("AzADUPN")]
-    [String]
-    ${Enter Azure AD Admin UPN},
-    [Parameter(Mandatory=$True)]
     [Alias("UserName")]
     [String]
     ${Enter UPN of User Token to Revoke}
     )
 
-Connect-Azuread -AccountId $AzADUPN
+Connect-Azuread
 
 $TokenStart=(Get-AzureADUser -SearchString "$UserName").RefreshTokensValidFromDateTime
 Write-host "$UserName original token created on $TokenStart"
 (Get-AzureADUser -SearchString "$UserName").ObjectId | Revoke-AzureADUserAllRefreshToken
 $NewToken=(Get-AzureADUser -SearchString "$UserName").RefreshTokensValidFromDateTime
 Write-host "$UserName new token created on $NewToken"
+
+Set-AzureADUser -ObjectId $UserName -AccountEnabled $false
+
+$UserDevices=(Get-AzureADUserRegisteredDevice -ObjectId $UserName).DisplayName
+
+Write-Host "$UserDevices will be disabled in Azure Active Directory"
+
+Get-AzureADUserRegisteredDevice -ObjectId $UserName | Set-AzureADDevice -AccountEnabled $false
